dbb8848cac9fcda07ae26647e891d87c0e5c3893
Hugi Hlynsson <hugihlynsson@gmail.com>
2016-03-25 15:24:53 +0000

    Add inputs and ranges to update it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Typer from './Typer';
 import styles from './App.css';
 
-
-const App = () => (
-  <span>
-    <h1 className={styles.root}>Hello, world.</h1>
-  </span>
-);
-
-export default App;
+// Don't do stateless for hot-reloading
+// eslint-disable-next-line react/prefer-stateless-function
+export default class App extends Component {
+  render() {
+    return (
+      <span>
+        <h1 className={styles.header}>Grotest 123</h1>
+        <Typer name="Test1" />
+        <Typer name="Test2" />
+        <Typer name="Test3" />
+      </span>
+    );
+  }
+}
