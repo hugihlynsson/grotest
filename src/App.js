0e624c374af96c83cf8e0e746cd6f011a9d460d6
Hugi Hlynsson <hugihlynsson@gmail.com>
2016-03-25 14:02:29 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './App.css';
+
+
+const App = () => (
+  <span>
+    <h1 className={styles.root}>Hello, world.</h1>
+  </span>
+);
+
+export default App;
