139abe4cb429b5311e8a4e5a42817fd67ce27395
Hugi Hlynsson <hugihlynsson@gmail.com>
2016-04-10 14:11:05 +0000

    ES6ify server

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const config = require('./webpack.config');
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
-  historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+  historyApiFallback: true,
+}).listen(3000, 'localhost', (err) => {
   if (err) {
-    return console.log(err);
+    console.log(err);
+    return;
   }
 
   console.log('Listening at http://localhost:3000/');
