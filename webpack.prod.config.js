c9758562937d9059a6b334820abf0aaedd2621f7
Hugi Hlynsson <hugihlynsson@gmail.com>
2016-04-19 21:04:56 +0000

    Create production bundle

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,26 +1,19 @@
 const path = require('path');
-const webpack = require('webpack');
 
 module.exports = {
-  devtool: 'eval',
+  devtool: 'cheap-module-source-map',
   entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
     './src/index',
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/',
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-  ],
   module: {
     loaders: [
       {
         test: /\.js$/,
-        loaders: ['react-hot', 'babel'],
+        loaders: ['babel'],
         include: path.join(__dirname, 'src'),
       },
       {
